0b083390598656180a0d6e17d954909151708c75
DEV2@gmail.com <ponnala srinivas@LAPTOP-0IFL4VAS>
2020-05-03 22:11:17 +0530

    TL commit

--- NITCitiBank/src/com/nt/test/Test.java
+++ NITCitiBank/src/com/nt/test/Test.java
@@ -1,9 +1,12 @@
 package com.nt.test;
 
+import com.nt.beans.Utility;
+
 public class Test {
-	public void msg() {
+	public static void main(String[] args) {
 		System.out.println("Test.msg()");
 	System.out.println("hello world");
-	
+	Utility u=new Utility();
+	System.out.println(u.getSysDate());
 	}
 }
